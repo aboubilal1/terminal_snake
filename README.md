4e7467c38ad426c108c754296d48f771ab128fd7
aboubilal1 <62067726+aboubilal1@users.noreply.github.com>
2026-02-18 22:39:07 +0100

    program is done

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-terminal_snake"
+terminal_snake
 
-
-There is a problem starting at line 137 in the sanke.c file 
-
-
+- use the wasd to move
+- space to add body_part
+- esc to exit
--- dir_queue.c
+++ dir_queue.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "dir_queue.h"
 
 void init_queue(queue **q){
@@ -63,69 +62,3 @@ void tail_queue(queue *q, int *data){
     }
     *data = q->tail->data;
 }
-=======
-#include "dir_queue.h"
-
-void init_queue(queue **q){
-    // printf("5 Initializing queue.\n");
-    *q = malloc(sizeof(queue));
-    if (!(*q)) return;
-    // printf("5.1 Queue initialized.\n");
-    (*q)->head = NULL;
-    (*q)->tail = NULL;
-}
-
-int empty_queue(queue *q){
-    if (!q){
-        return 1;
-    }
-    return q->head == NULL;
-}
-
-int one_element_queue(queue *q){
-    if (!q){
-        return 1;
-    }
-    return q->head == q->tail;
-}
-
-void queue_in(queue *q, int data){
-    node *n = (node*)malloc(sizeof(node));
-    if (!n) return;
-    n->data = data;
-    n->next = NULL;
-
-    if(empty_queue(q)){
-        q->head = n;
-        q->tail = n;
-    }else{
-        q->tail->next = n;
-        q->tail = n;
-    }
-}
-
-void unqueue(queue *q, int *data){
-    if (empty_queue(q)){
-        return;
-    }
-    node *n = q->head;
-    if (!n) return;
-    *data = n->data;
-    q->head = q->head->next;
-    free(n);
-}
-
-void head_queue(queue *q, int *data){
-    if (empty_queue(q)){
-        return;
-    }
-    *data = q->head->data;
-}
-
-void tail_queue(queue *q, int *data){
-    if (empty_queue(q)){
-        return;
-    }
-    *data = q->tail->data;
-}
->>>>>>> ce7118a50dd3883eab2dbc8e32764ab92fb3fb8a
